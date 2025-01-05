85766720a88cb01ca89d53461c96e62b36b265b3
Rutuja <rutujamanware12@gmail.com>
2025-01-05 11:02:16 +0530

    Example commit

---
+++ Java/src/OopsConcept/EmployeeMain.java
@@ -0,0 +1,23 @@
+package OopsConcept; 
+
+public class EmployeeMain {
+
+	public static void main(String[] args) {
+		
+		 Employee emp1=new Employee(); //Object Create
+		  emp1.eid=101;
+		  emp1.ename="John";
+		  emp1.job="Manager";
+		  emp1.sal=500000;
+		  emp1.display();
+			
+		  Employee emp2=new Employee();
+		  emp2.eid=102;
+		  emp2.ename="David";
+		  emp2.job="Engineer";
+		  emp2.sal=800000;
+		  emp2.display();
+			
+	}
+
+}
---
+++ Java/src/OopsConcept/Student.java
@@ -0,0 +1,18 @@
+package OopsConcept;
+
+public class Student {
+        int sid;
+		String sname;
+		char grad;
+	
+		
+		void printData()
+		{
+			System.out.println(sid+"   "+sname+"   "+grad);
+			
+		}
+		
+
+	}
+
+
